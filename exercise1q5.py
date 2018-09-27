7241ee6ee5c378860f483ce16da4095f6815122c
kuiqejw <lauraong1@yahoo.com.sg>
2018-09-27 12:29:25 +0800

    Add files via upload

--- exercise1q5.py
+++ exercise1q5.py
@@ -1,28 +1,55 @@
 class MerkleTree(Object):
+    def __init__(self):
+        self.past_transactions = []
+        self.past_transaction_hashes = []
+        self.tiered_node_list = []
+        self.root = None
 
-    def add(self, values, do_hash=False):
+
+    def add(self, new_transaction):
+        new_hash_hex_digest = hashlib.sha512(new_transaction).hexdigest()
+        self.past_transactions.append(new_transaction)
+        self.past_transactions_hashes.append(new_hash_hex_digest)
+        print('add new transaction with hex digest: ', new_hash_hex_digest)
         # Add entries to tree
-        self.is_ready = False 
-        if not isinstance(values, tuple) and not isinstance(values, list):
-            values = [values]
-        for v in values:
-            if do_hash:
-                v = v.encode('utf-8')
-                v = self.hash_function(v).hexdigest()
-            v = bytearray.fromhex(v)
-            self.leaves.append(v)
+        
 
-    def build(self, hash_type = 'sha256'):
+    def build(self):
         # Build tree computing new root
-        hash_type = hash_type.lower()
-        if hash_type in ['sha256', 'md5', 'sha224', 'sha384', 'sha512',
-                         'sha3_256', 'sha3_224', 'sha3_384', 'sha3_512']:
-            self.hash_function = getattr(hashlib, hash_type)
-        else:
-            raise Exception('`hash_type` {} nor supported'.format(hash_type))
-        self.leaves = list()
-        self.levels = None
-        self.is_ready = False
+        num_leaves = len(self.past_transactions)#grab the number of leaves
+        remaining_nodes = num_leaves
+        active_level = self.past_transaction_hashes
+        while remaining_nodes != 1:
+            if remaining_nodes % 2:
+                odd = False
+            else:
+                odd = True
+
+            new_tier = []
+            for i in range(1, remaining_nodes,2):
+                #create the new tier on top of active_level -1 and active leve current hash
+                combined_str = str(active_level[i-1]) + str(active_level[i])
+                new_hash = hashlib.sha512(combined_str.encode()).hexdigest()
+                new_tier.append(new_hash)
+            if odd:
+                #
+                new_tier.append(active_level[num_leaves-1].encode())
+            self.tiered_node_list.append(new_tier)
+            remaining_nodes = len(new_tier)
+            active_level = new_tier
+
+        self.tiered_node_list.insert(0,self.past_transaction_hashes) #insert in front of previous hashes
+        self.root = self.tiered_node_list[-1] #seize root from back of tiered node list
+        print('Tree build complete\n'  + 'No. of levels: ', len(self.tiered_node_list))
+        # hash_type = hash_type.lower()
+        # if hash_type in ['sha256', 'md5', 'sha224', 'sha384', 'sha512',
+        #                  'sha3_256', 'sha3_224', 'sha3_384', 'sha3_512']:
+        #     self.hash_function = getattr(hashlib, hash_type)
+        # else:
+        #     raise Exception('`hash_type` {} nor supported'.format(hash_type))
+        # self.leaves = list()
+        # self.levels = None
+        # self.is_ready = False
 
     def get_proof(self, index):
         # Get membership proof for entry
@@ -48,32 +75,10 @@ class MerkleTree(Object):
         return hex(self.leaves[index])
     def get_root(self):
         # Return the current root
-        if self.is_ready:
-            if self.levels is not None:
-                return hex(self.levels[0][0])
-            else:
-                return None
-        else:
-        return None
-    def validate_proof(self, proof, target_hash, merkle_root):
-        merkle_root = bytearray.fromhex(merkle_root)
-        target_hash = bytearray.fromhex(target_hash)
-        if len(proof) == 0:
-            return target_hash == merkle_root
-        else:
-            proof_hash = target_hash
-            for p in proof:
-                try:
-                    # the sibling is a left node
-                    sibling = bytearray.fromhex(p['left'])
-                    proof_hash = self.hash_function(sibling + proof_hash).digest()
-                except:
-                    # the sibling is a right node
-                    sibling = bytearray.fromhex(p['right'])
-                    proof_hash = self.hash_function(proof_hash + sibling).digest()
-        return proof_hash == merkle_root
-
-def verify_proof(entry, proof, root):
-    # Verifies proof for entry and given root. Returns boolean.
-    mt = MerkleTools(hash_type = 'md5')
-    mt.validate_proof(proof, entry,root)
+        if not self.root:
+            print('No root found')
+            return None
+        return self.tiered_node_list[-1]
+    
+def verify_proof():
+    pass
